422b11183e638c5852f51ca4dc747a32bf8b29ae
DonNavinS <seneviratnanavin@gmail.com>
2021-05-21 23:34:52 -0400

    Added button functionality
    Button now saves text to local storage and can be retrieved using dynamic HTML

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,26 @@
 var headerEl = document.querySelector("#currentDay");
 var inputEl9 = document.querySelector("#input9")
 var buttonEl9 = document.querySelector("#button9")
+var displayEl9 = document.querySelector("#reminder")
+displayEl9.className = "display9"
 
 
 var currentTime = document.createElement("p");
 currentTime.textContent = moment().format("MMMM Do YYYY");
 
 headerEl.appendChild(currentTime);
+
+
+var timeOfChange = moment().format('MMMM Do YYYY, h:mm:ss a')
+
+buttonEl9.addEventListener("click", function() {
+    localStorage.setItem(timeOfChange, inputEl9.value);
+})
+
+
+if (localStorage.key("reminder") !== null){
+    displayEl9.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
+}
+
 
 
