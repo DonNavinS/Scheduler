bad48aaa32b602daecd53611cf50699e3821e6b1
DonNavinS <seneviratnanavin@gmail.com>
2021-05-23 22:26:47 -0400

    CSS corresponds to time of day and fixed saving
    Used moment.js to change the CSS of the blocks based on what time of day it is. Also modified the code used for saving and recalling information from local storage.

--- Develop/script.js
+++ Develop/script.js
@@ -37,135 +37,12 @@ displayEl4.className = "display4"
 displayEl5.className = "display5"
 var deleteButtonEl = document.querySelector("#delete-button")
 
+
 // Display Current Date at the top of the page
-var currentTime = document.createElement("p");
-currentTime.textContent = moment().format("MMMM Do YYYY");
+var currentDate = document.createElement("p");
+currentDate.textContent = moment().format("MMMM Do YYYY");
 
-headerEl.appendChild(currentTime);
-
-
-// Save content to scheduler
-var timeOfChange = moment().format('MMMM Do YYYY, h:mm:ss')
-
-buttonEl9.addEventListener("click", function() {
-    localStorage.setItem(timeOfChange, inputEl9.value);
-
-
-if (localStorage.length !== 0 ) {
-displayEl9.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-};
-
-})
-
-if (localStorage.length !== 0 ) {
-    displayEl9.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-}
-
-buttonEl10.addEventListener("click", function() {
-    localStorage.setItem(timeOfChange, inputEl10.value);
-
-
-if (localStorage.length !== 0 ) {
-displayEl10.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-};
-})
-
-if (localStorage.length !== 0 ) {
-    displayEl10.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-    };
-
-
-buttonEl11.addEventListener("click", function() {
-    localStorage.setItem(timeOfChange, inputEl11.value);
-
-
-if (localStorage.length !== 0 ) {
-displayEl11.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-};
-})
-
-if (localStorage.length !== 0 ) {
-    displayEl11.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-    };
-
-buttonEl12.addEventListener("click", function() {
-    localStorage.setItem(timeOfChange, inputEl12.value);
-
-
-if (localStorage.length !== 0 ) {
-displayEl12.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-};
-})
-
-if (localStorage.length !== 0 ) {
-    displayEl12.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-    };
-
-
-buttonEl1.addEventListener("click", function() {
-    localStorage.setItem(timeOfChange, inputEl1.value);
-
-
-if (localStorage.length !== 0 ) {
-displayEl1.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-};
-})
-
-if (localStorage.length !== 0 ) {
-    displayEl1.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-    };
-
-buttonEl2.addEventListener("click", function() {
-    localStorage.setItem(timeOfChange, inputEl2.value);
-
-
-if (localStorage.length !== 0 ) {
-displayEl2.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-};
-})
-
-if (localStorage.length !== 0 ) {
-    displayEl2.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-    };
-
-buttonEl3.addEventListener("click", function() {
-    localStorage.setItem(timeOfChange, inputEl3.value);
-
-
-if (localStorage.length !== 0 ) {
-displayEl3.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-};
-})
-
-if (localStorage.length !== 0 ) {
-    displayEl3.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-    };
-
-buttonEl4.addEventListener("click", function() {
-    localStorage.setItem(timeOfChange, inputEl4.value);
-
-
-if (localStorage.length !== 0 ) {
-displayEl4.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-};
-})
-
-if (localStorage.length !== 0 ) {
-    displayEl4.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-    };
-
-buttonEl5.addEventListener("click", function() {
-    localStorage.setItem(timeOfChange, inputEl5.value);
-
-
-if (localStorage.length !== 0 ) {
-displayEl5.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-};
-})
-
-if (localStorage.length !== 0 ) {
-    displayEl5.innerHTML += localStorage.getItem(localStorage.key(timeOfChange));
-    };
+headerEl.appendChild(currentDate);
 
 
 
@@ -174,3 +51,271 @@ deleteButtonEl.addEventListener("click", function() {
     localStorage.clear();
     location.reload()
 })
+
+
+// Change background based on time of day
+var timeNow = moment().format("H")
+
+
+// From 9:00AM-9:59AM
+if (timeNow == 9) {
+    inputEl9.className = "present";
+    inputEl10.className = "future";
+    inputEl11.className = "future";
+    inputEl12.className = "future";
+    inputEl1.className = "future";
+    inputEl2.className = "future";
+    inputEl3.className = "future";
+    inputEl4.className = "future";
+    inputEl5.className = "future";
+}
+
+if (timeNow == 10) {
+    inputEl9.className = "past";
+    inputEl10.className = "present";
+    inputEl11.className = "future";
+    inputEl12.className = "future";
+    inputEl1.className = "future";
+    inputEl2.className = "future";
+    inputEl3.className = "future";
+    inputEl4.className = "future";
+    inputEl5.className = "future";
+}
+
+if (timeNow == 11) {
+    inputEl9.className = "past";
+    inputEl10.className = "past";
+    inputEl11.className = "present";
+    inputEl12.className = "future";
+    inputEl1.className = "future";
+    inputEl2.className = "future";
+    inputEl3.className = "future";
+    inputEl4.className = "future";
+    inputEl5.className = "future";
+}
+
+if (timeNow == 12) {
+    inputEl9.className = "past";
+    inputEl10.className = "past";
+    inputEl11.className = "past";
+    inputEl12.className = "present";
+    inputEl1.className = "future";
+    inputEl2.className = "future";
+    inputEl3.className = "future";
+    inputEl4.className = "future";
+    inputEl5.className = "future";
+}
+
+if (timeNow == 13) {
+    inputEl9.className = "past";
+    inputEl10.className = "past";
+    inputEl11.className = "past";
+    inputEl12.className = "past";
+    inputEl1.className = "present";
+    inputEl2.className = "future";
+    inputEl3.className = "future";
+    inputEl4.className = "future";
+    inputEl5.className = "future";
+}
+
+if (timeNow == 14) {
+    inputEl9.className = "past";
+    inputEl10.className = "past";
+    inputEl11.className = "past";
+    inputEl12.className = "past";
+    inputEl1.className = "past";
+    inputEl2.className = "present";
+    inputEl3.className = "future";
+    inputEl4.className = "future";
+    inputEl5.className = "future";
+}
+
+if (timeNow == 15) {
+    inputEl9.className = "past";
+    inputEl10.className = "past";
+    inputEl11.className = "past";
+    inputEl12.className = "past";
+    inputEl1.className = "past";
+    inputEl2.className = "past";
+    inputEl3.className = "present";
+    inputEl4.className = "future";
+    inputEl5.className = "future";
+}
+
+if (timeNow == 16) {
+    inputEl9.className = "past";
+    inputEl10.className = "past";
+    inputEl11.className = "past";
+    inputEl12.className = "past";
+    inputEl1.className = "past";
+    inputEl2.className = "past";
+    inputEl3.className = "past";
+    inputEl4.className = "present";
+    inputEl5.className = "future";
+}
+
+if (timeNow == 17) {
+    inputEl9.className = "past";
+    inputEl10.className = "past";
+    inputEl11.className = "past";
+    inputEl12.className = "past";
+    inputEl1.className = "past";
+    inputEl2.className = "past";
+    inputEl3.className = "past";
+    inputEl4.className = "past";
+    inputEl5.className = "present";
+}
+
+if (timeNow < 9) {
+    inputEl9.className = "future";
+    inputEl10.className = "future";
+    inputEl11.className = "future";
+    inputEl12.className = "future";
+    inputEl1.className = "future";
+    inputEl2.className = "future";
+    inputEl3.className = "future";
+    inputEl4.className = "future";
+    inputEl5.className = "future";
+}
+
+if (timeNow > 17) {
+    inputEl9.className = "past";
+    inputEl10.className = "past";
+    inputEl11.className = "past";
+    inputEl12.className = "past";
+    inputEl1.className = "past";
+    inputEl2.className = "past";
+    inputEl3.className = "past";
+    inputEl4.className = "past";
+    inputEl5.className = "past";
+}
+
+
+// Save content to scheduler
+var timeOfChange = moment().format('MMMM Do YYYY, h:mm:ss')
+
+
+buttonEl9.addEventListener("click", function() {
+    localStorage.setItem("block 9", inputEl9.value);
+
+// First if statement displays saved text right when "Save" button is clicked, the second if 
+// statement displays the saved text when the page is refreshed
+
+
+if (localStorage.getItem("block 9") !== null) {
+displayEl9.innerHTML += localStorage.getItem("block 9");
+};
+
+})
+
+if (localStorage.getItem("block 9") !== null) {
+    displayEl9.innerHTML += localStorage.getItem("block 9");
+    }
+
+buttonEl10.addEventListener("click", function() {
+    localStorage.setItem("block 10", inputEl10.value);
+
+
+if (localStorage.getItem("block 10") !== null ) {
+displayEl10.innerHTML += localStorage.getItem("block 10");
+};
+})
+
+if (localStorage.getItem("block 10") !== null ) {
+    displayEl10.innerHTML += localStorage.getItem("block 10");
+    };
+
+
+buttonEl11.addEventListener("click", function() {
+    localStorage.setItem("block 11", inputEl11.value);
+
+
+if (localStorage.getItem("block 11") !== null) {
+displayEl11.innerHTML += localStorage.getItem("block 11");
+};
+})
+
+if (localStorage.getItem("block 11") !== null) {
+    displayEl11.innerHTML += localStorage.getItem("block 11");
+    };
+
+buttonEl12.addEventListener("click", function() {
+    localStorage.setItem("block 12", inputEl12.value);
+
+
+if (localStorage.getItem("block 12") !== null) {
+displayEl12.innerHTML += localStorage.getItem("block 12");
+};
+})
+
+if (localStorage.getItem("block 12") !== null) {
+    displayEl12.innerHTML += localStorage.getItem("block 12");
+    };
+
+
+buttonEl1.addEventListener("click", function() {
+    localStorage.setItem("block 1", inputEl1.value);
+
+
+if (localStorage.getItem("block 1") !== null) {
+displayEl1.innerHTML += localStorage.getItem("block 1");
+};
+})
+
+if (localStorage.getItem("block 1") !== null) {
+    displayEl1.innerHTML += localStorage.getItem("block 1");
+    };
+
+buttonEl2.addEventListener("click", function() {
+    localStorage.setItem("block 2", inputEl2.value);
+
+
+if (localStorage.getItem("block 2") !== null) {
+displayEl2.innerHTML += localStorage.getItem("block 2");
+};
+})
+
+if (localStorage.getItem("block 2") !== null) {
+    displayEl2.innerHTML += localStorage.getItem("block 2");
+    };
+
+buttonEl3.addEventListener("click", function() {
+    localStorage.setItem("block 3", inputEl3.value);
+
+
+if (localStorage.getItem("block 3") !== null) {
+displayEl3.innerHTML += localStorage.getItem("block 3");
+};
+})
+
+if (localStorage.getItem("block 3") !== null) {
+    displayEl3.innerHTML += localStorage.getItem("block 3");
+    };
+
+buttonEl4.addEventListener("click", function() {
+    localStorage.setItem("block 4", inputEl4.value);
+
+
+if (localStorage.getItem("block 4") !== null) {
+displayEl4.innerHTML += localStorage.getItem("block 4");
+};
+})
+
+if (localStorage.getItem("block 4") !== null) {
+    displayEl4.innerHTML += localStorage.getItem("block 4");
+    };
+
+buttonEl5.addEventListener("click", function() {
+    localStorage.setItem("block 5", inputEl5.value);
+
+
+if (localStorage.getItem("block 5") !== null) {
+displayEl5.innerHTML += localStorage.getItem("block 5");
+};
+})
+
+if (localStorage.getItem("block 5") !== null) {
+    displayEl5.innerHTML += localStorage.getItem("block 5");
+    };
+
+
